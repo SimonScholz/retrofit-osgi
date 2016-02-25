921f93e0c14abeae9181be88d2e0402d10493461
Simon Scholz <simon.scholz@vogella.com>
2016-02-25 20:05:06 +0100

    Removed obsolete split package resolver bundle
    Change-Id: I005a1cacf7d7596686bf49cee83cbdaf82ec7a13
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- retrofit-osgi/src/retrofit/OSGiRetrofitPackage.java
+++
@@ -1,9 +0,0 @@
-package retrofit;
-
-final class OSGiRetrofitPackage {
-	private OSGiRetrofitPackage() {
-		// this is just a placeholder class, so that Eclipse does not complain
-		// about an empty package being reexported.
-		// See "Runtime" tab in the MANIFEST.MF editor
-	}
-}
